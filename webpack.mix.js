74ced6c826773320853c2d627a07189344bcf771
pasapas <pcpasapas@gmail.com>
2022-02-18 21:21:27 +0100

    nouveau

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/scss/app.scss', 'public/css')
     .sourceMaps();
