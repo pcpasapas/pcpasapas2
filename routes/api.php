528ad76a3f876bc4c0f030b3093cb494115ece2b
pasapas <pcpasapas@gmail.com>
2022-02-28 06:42:57 +0100

    cmbdd

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlimentationsController;
 use App\Http\Controllers\BoitiersController;
+use App\Http\Controllers\CarteMereController;
 use App\Http\Controllers\ProcesseursController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 
 /* |-------------------------------------------------------------------------- | API Routes |-------------------------------------------------------------------------- | | Here is where you can register API routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | is assigned the "api" middleware group. Enjoy building your API! | */
@@ -12,6 +13,7 @@ Route::get('alimentations', [AlimentationsController::class , 'show']);
 Route::get('alimentationsshowGTA', [AlimentationsController::class , 'showGTA']);
 Route::get('boitiers', [BoitiersController::class , 'show']);
 Route::get('processeurs', [ProcesseursController::class , 'show']);
+Route::get('cartemere', [CarteMereController::class , 'show']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
